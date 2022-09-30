f89493911016eeb754f7027914f89d1528bd7017
rimtimti <rimtimti@mail.ru>
2022-09-30 12:50:23 +0300

    1

--- check.py
+++ check.py
@@ -12,17 +12,13 @@ def check_telephone_number(symbol: str) -> str:
         except ValueError:
             print('Ошибка! Введены некорректные символы!')
 
-# check_choice_one_or_two
-def check_int_number(digit: str) -> int:
+
+def check_int_number(number: str) -> int:
     '''
-    Проверка ввода 1 или 2
+    Поверка на целое число
     '''
     while True:
         try:
-            check_int_number = int(input(digit))
-            if check_int_number == 1 or check_int_number == 2:
-                return check_int_number
-            else:
-                print('Ошибка! Для выбора калькулятора рациональных чисел нажмите 1, для комплексных - 2')
+            return int(input(number))
         except ValueError:
-            print('Ошибка! Для выбора калькулятора рациональных чисел нажмите 1, для комплексных - 2')
+            print('Ошибка! Должно быть целое число!')
